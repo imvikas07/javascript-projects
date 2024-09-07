ce8968565c384cfe8f09953f3f629b97f801a1c4
Vikas <vikas@Vikass-MacBook-Pro.local>
2024-09-07 16:15:02 +0530

    update content

--- project-3-Digital-clock/clock.js
+++
@@ -1,8 +0,0 @@
-const displayClock = document.querySelector('#clock');
-
-setInterval(function(){
-    let time = new Date();
-    displayClock.innerHTML = time.toLocaleTimeString();
-    displayClock.style.color = 'black';  // Change the color of the clock
-    displayClock.style.fontSize = '20px'; // Increase the font size
-}, 1000);
